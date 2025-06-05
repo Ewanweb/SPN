f049d96125faa3443038e9720f4e45758e0882c9
Mahan <Mahan001ms@gmail.com>
2025-06-05 21:21:23 +0430

    Edit Project UseCase

---
+++ Site/Site.Application/Projects/Edit/EditProjectCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Site.Application._shared;
+
+namespace Site.Application.Projects.Edit
+{
+    public record EditProjectCommand(string ProjectSlug, string Title, string Description, IFormFile? Image) : IRequest<OperationResult>;
+}
---
+++ Site/Site.Application/Projects/Edit/EditProjectCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Site.Application._shared;
+using Site.Application._shared.FileUtil.Interfaces;
+using Site.Domain._shared;
+using Site.Domain.Repositories;
+
+namespace Site.Application.Projects.Edit;
+
+public class EditProjectCommandHandler : IRequestHandler<EditProjectCommand, OperationResult>
+{
+    private readonly IProjectRepository _repository;
+    private readonly IFileService _fileService;
+
+    public EditProjectCommandHandler(IProjectRepository repository, IFileService fileService)
+    {
+        _repository = repository;
+        _fileService = fileService;
+    }
+
+    public async Task<OperationResult> Handle(EditProjectCommand request, CancellationToken cancellationToken)
+    {
+        var project = await _repository.GetProjectBySlug(request.ProjectSlug);
+
+        if (project is null)
+            return OperationResult.Error("پروژه ای با این مشخصات پیدا نشد");
+
+
+        try
+        {
+
+            string imageName;
+
+            if (request.Image is null)
+                imageName = project.MainImageName;
+            else
+                imageName = await _fileService.SaveFileAndGenerateName(request.Image, Directories.ProjectImages);
+
+            var slug = SlugHelper.GenerateSlug(request.Title);
+
+            project.Edit(request.Title, slug, request.Description, imageName);
+
+            await _repository.SaveChangesAsync();
+            return OperationResult.Success("پروژه با موفقیت ویرایش شد");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return OperationResult.Error("عملیات شکست خورد");
+        }
+    }
+}

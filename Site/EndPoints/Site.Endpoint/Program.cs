9a463aa4d4712c592bce728ac7e2243cdd9fd3b3
Mahan <Mahan001ms@gmail.com>
2025-07-21 13:14:38 +0330

    Update

--- Site/EndPoints/Site.Endpoint/Program.cs
+++ Site/EndPoints/Site.Endpoint/Program.cs
@@ -12,11 +12,6 @@ ConfigBootstrapper.Init(builder.Services, builder.Configuration);
 var app = builder.Build();
 
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate(); // ← اجرای مایگریشن‌ها
-}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
